d7d8f3f23585c058c8a2498db006befdd5724dff
Emmanuel HURET <huret.emmanuel@infoatoutprix.fr>
2019-05-13 01:50:06 +0200

    Test6 js

--- delivery_direct_print/models/stock_picking.py
+++ delivery_direct_print/models/stock_picking.py
@@ -46,8 +46,8 @@ class StockPicking(models.Model):
         document = ""
         for label in labels:
             if label.datas:
-                _logger.info(label.datas)
-                _logger.info(label.datas.decode('base64'))
+                _logger.info(label.with_context(bin_size=False).datas)
+                _logger.info(label.with_context(bin_size=False).datas.decode('base64'))
                 document += label.datas.decode('base64')
         for rec in self:
             rec.label_zpl = document
